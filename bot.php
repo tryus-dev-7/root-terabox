99415c8144c7615f99eb294124ea59ded9a2ceb4
TRY DEV7 <tryus.dev7@gmail.com>
2024-11-05 10:14:55 +0600

    Bot V1.998

--- bot.php
+++ bot.php
@@ -114,9 +114,6 @@ if (isset($update['message'])) {
                 ]
             ];
 
-            sendMessage($chatId, "ID : $videoId", $keyboard, parseMode: "Markdown");
-
-
             sendMessage($chatId, "*➡️ Title :* $title\n\n_Choose an option below:_", $keyboard, "Markdown");
         } else {
             // Delete generating message if it was sent
