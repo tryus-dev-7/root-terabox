6b87d98c9f0f44a802124417128dfa3babefece3
TRY DEV7 <tryus.dev7@gmail.com>
2024-11-05 12:21:47 +0600

    Bot V2.41

--- bot.php
+++ bot.php
@@ -141,7 +141,7 @@ if (isset($update['message'])) {
 
 
 
-                sendMessage($chatId, "*➡️ Title :* $title\n\n_Choose an option below:_", null, "Markdown");
+                sendMessage($chatId, "*➡️ Title :* $title\n\n _Choose an option below:_", null, "Markdown");
             } else {
                 // Delete generating message if it was sent
                 if (isset($genMessage['result'])) {
